047a2dad01f785abdcfecc837cc97d8fa440825e
Markus Binsteiner <markus.binsteiner@arcs.org.au>
2009-04-24 00:31:17 +0000

    Fixed javadoc.
    git-svn-id: http://projects.arcs.org.au/svn/grix/trunk/proxy@291 75d53db5-b72d-0410-9bdf-f62711b48035

--- src/main/java/org/vpac/security/light/utils/ActionPerformedListener.java
+++ src/main/java/org/vpac/security/light/utils/ActionPerformedListener.java
@@ -4,8 +4,9 @@ public interface ActionPerformedListener {
 
 	/**
 	 * Is activated when the child object successfully (or not) performed it's intended task.
+	 * @param actionName the name of the action
 	 * @param success true - if successfully; false - if not
-	 * @param objects to pass to the listener (the listener has to know what to expect)
+	 * @param params to pass to the listener (the listener has to know what to expect)
 	 */
 	public void success( String actionName, boolean success, Object[] params);
 	
