a39213e9da5329e3d5543019ab98061e859f570a
Yuriy Halytskyy <y.halytskyy@auckland.ac.nz>
2012-09-26 15:46:27 +1200

    implemented kerberos myproxyca login

---
+++ src/main/java/grith/jgrith/kerberos/MyProxyAuthException.java
@@ -0,0 +1,14 @@
+package grith.jgrith.kerberos;
+
+public class MyProxyAuthException extends Exception {
+	private static final long serialVersionUID = 1900255085586758233L;
+	
+	public MyProxyAuthException(Exception e){
+		super(e);
+	}
+	
+	public MyProxyAuthException(String m){
+		
+	}
+
+}
